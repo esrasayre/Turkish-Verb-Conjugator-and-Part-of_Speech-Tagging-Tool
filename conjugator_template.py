a351c3f84a5fa1d72f9ad4a9fe1586efbe56685d
Esra Arslan <arslan.esra@protonmail.com>
2023-01-25 00:12:12 +0300

    Create conjugator_template.py

---
+++ conjugator_template.py
@@ -0,0 +1,18 @@
+class TurkishVerbConjugator:
+    def __init__(self, infinitive_verb):
+        self.infinitive_verb = infinitive_verb
+
+    def conjugate(self, person, number, tense):
+        """
+        Conjugates the infinitive verb according to the given person, number, and tense.
+        person: 1 for first person, 2 for second person, 3 for third person
+        number: 1 for singular, 2 for plural
+        tense: "present", "past", "future"
+        """
+        # Implement the rules of suffixation and phonetic changes here
+        pass
+It should be noted that, this is a basic shell of a verb conjugator, you need to implement the rules of suffixation and phonetic changes as well as the logic for different tenses and conjugations. It is a non-trivial task and requires a good understanding of Turkish grammar, and it is recommended to consult a professional.
+
+
+
+
